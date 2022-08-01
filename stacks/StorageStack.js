016bcd199ed4bd0478a2ee871809254735200dc5
KNS786 <doolocompany@gmail.com>
2022-08-01 15:42:11 +0530

    create dynamo db in aws

---
+++ stacks/StorageStack.js
@@ -0,0 +1,14 @@
+import {Table} from '@serverless-stack/resources';
+
+export function StorageStack({stack,app}){
+    const table = new Table(stack,"Notes",{
+        fields:{
+            userId:"string",
+            noteId:"string"
+        },
+        primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+    });
+    return {
+        table
+    }
+}
